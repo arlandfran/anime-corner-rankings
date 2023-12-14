b0437bc587f22f282b0dd7ff2392934106b5ea11
arlandfran <arland.torres@outlook.com>
2023-12-14 03:46:52 +0000

    Add dark mode toggle
    - also adjust header padding and add shadows for light mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+  import { ModeWatcher } from "mode-watcher";
   import Header from "$lib/components/ui/header.svelte";
   import "../app.pcss";
 </script>
 
+<ModeWatcher />
 <Header />
 
 <slot />
