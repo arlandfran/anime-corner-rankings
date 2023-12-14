3b1ff2cb1193dbb47f85791ed40ac7aabbc72317
arlandfran <arland.torres@outlook.com>
2023-12-14 21:08:57 +0000

    Add padding to main container

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,6 @@
 
 <ModeWatcher />
 <Header />
-<main class="container flex min-w-[14rem] max-w-xl flex-col items-center justify-center gap-4">
+<main class="container flex min-w-[14rem] max-w-xl flex-col items-center justify-center gap-4 py-6">
   <slot />
 </main>
