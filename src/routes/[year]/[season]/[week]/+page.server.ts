5911752b0724cf5d289422616104fa7c909d490e
arlandfran <arland.torres@outlook.com>
2023-12-14 19:36:04 +0000

    Get rankings from db on seasonId
    - query returns rank, anilistId, votes, previousRank, rankDifference and
      votesDifference

--- src/routes/[year]/[season]/[week]/+page.server.ts
+++ src/routes/[year]/[season]/[week]/+page.server.ts
@@ -1,10 +1,80 @@
 import { db } from "$lib/server/database";
 import { ranking } from "$lib/server/schema";
-import { count } from "drizzle-orm";
+import { asc, eq, sql } from "drizzle-orm";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async () => {
-  const [{ value }] = await db.select({ value: count() }).from(ranking);
+interface PageParams {
+  year: string;
+  season: string;
+  week: string;
+}
 
-  return { count: value };
+function parseParams(params: PageParams) {
+  const { year, season, week } = params;
+  switch (season) {
+    case "winter":
+      return parseInt(year.substring(2) + "1" + week);
+    case "spring":
+      return parseInt(year.substring(2) + "2" + week);
+    case "summer":
+      return parseInt(year.substring(2) + "3" + week);
+    case "fall":
+      return parseInt(year.substring(2) + "4" + week);
+    default:
+      throw new Error(
+        `Invalid params. Season must be winter, spring, fall, summer - season: ${season}`,
+      );
+  }
+}
+
+async function getRankings(seasonId: number) {
+  const query = db
+    .select({
+      rank: ranking.rank,
+      anilistId: ranking.anilistId,
+      votes: ranking.votes,
+    })
+    .from(ranking)
+    .where(eq(ranking.seasonId, seasonId))
+    .as("query");
+
+  const previous = db
+    .select({
+      rank: ranking.rank,
+      anilistId: ranking.anilistId,
+      votes: ranking.votes,
+    })
+    .from(ranking)
+    .where(
+      eq(
+        ranking.seasonId,
+        // this one liner converts 231209 to 23129 when decrementing seasonId
+        seasonId % 10 === 0 ? parseInt(seasonId.toString().slice(0, -2) + "9") : seasonId - 1,
+      ),
+    )
+    .as("previous");
+
+  const prepared = db
+    .select({
+      rank: query.rank,
+      anilistId: query.anilistId,
+      votes: query.votes,
+      previousRank: sql<number>`coalesce(previous.rank, null)`,
+      rankDifference: sql<number>`coalesce(previous.rank - query.rank, null)`,
+      votesDifference: sql<number>`coalesce(round(query.votes - previous.votes,2), null)`,
+    })
+    .from(query)
+    .leftJoin(previous, eq(query.anilistId, previous.anilistId))
+    .orderBy(asc(query.rank))
+    .limit(10)
+    .prepare();
+
+  return await prepared.all();
+}
+
+export const load: PageServerLoad = async ({ params }) => {
+  const seasonId = parseParams(params);
+  const rankings = await getRankings(seasonId);
+
+  return { rankings: rankings };
 };
--- src/routes/[year]/[season]/[week]/+page.svelte
+++ src/routes/[year]/[season]/[week]/+page.svelte
@@ -5,8 +5,19 @@
   export let data: PageData;
 
   $: ({ year, season, week } = $page.params);
+  $: ({ rankings } = data);
 </script>
 
 <h1 class="text-center capitalize">{season} {year} - week {week}</h1>
 
-<p class="text-center">Count: {data.count}</p>
+<ol class="text-center">
+  {#each rankings as ranking}
+    <li>
+      {ranking.rank}. {ranking.anilistId}
+      {ranking.previousRank}
+      {ranking.rankDifference}
+      {ranking.votes}
+      {ranking.votesDifference}
+    </li>
+  {/each}
+</ol>
