6142a5e5c853c44aa58dfbfe6502b166d71a1920
arlandfran <arland.torres@outlook.com>
2025-04-18 12:31:21 +0100

    Query latest weekly ranking and display on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+import { getWeeklyRankings } from "@/lib/db";
+
+export default async function Home() {
+  const latestRanking = await getWeeklyRankings(2023, "fall", 1);
   return (
     <main>
-      <div>Hello world!</div>
+      {latestRanking.map((anime) => (
+        <div key={anime.animeId} className="flex gap-4">
+          <h2>{anime.animeTitle}</h2>
+          <p>Rank: {anime.rank}</p>
+          <p>Votes: {anime.votes}</p>
+          <p>Rank Delta: {anime.rankDelta}</p>
+          <p>Votes Delta: {anime.votesDelta}</p>
+        </div>
+      ))}
     </main>
   );
 }
--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,5 +1,7 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
+import { anime, weeklyRanking } from "./schema";
+import { eq, and, asc } from "drizzle-orm";
 
 const client = createClient({
   url: process.env.TURSO_DATABASE_URL!,
@@ -7,3 +9,35 @@ const client = createClient({
 });
 
 export const db = drizzle(client);
+
+export const getWeeklyRankings = async (
+  year: number,
+  season: string,
+  week: number
+) => {
+  const result = await db
+    .select({
+      animeId: weeklyRanking.animeId,
+      year: weeklyRanking.year,
+      season: weeklyRanking.season,
+      week: weeklyRanking.week,
+      rank: weeklyRanking.rank,
+      rankDelta: weeklyRanking.rankDelta,
+      votes: weeklyRanking.votes,
+      votesDelta: weeklyRanking.votesDelta,
+      animeTitle: anime.title,
+    })
+    .from(weeklyRanking)
+    .leftJoin(anime, eq(weeklyRanking.animeId, anime.animeId))
+    .where(
+      and(
+        eq(weeklyRanking.year, year),
+        eq(weeklyRanking.season, season),
+        eq(weeklyRanking.week, week)
+      )
+    )
+    .orderBy(asc(weeklyRanking.rank))
+    .limit(10);
+
+  return result;
+};
